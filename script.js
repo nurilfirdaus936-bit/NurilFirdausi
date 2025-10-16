d80299795a114b581a5f21c9dda3cc3b7d1d8d0d
nurilfirdaus936-bit <nurilfirdaus936@gmail.com>
2025-10-16 13:35:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const tombolSapa = document.getElementById('sapaButton');
 tombolSapa.addEventListener('click', function() {
-  alert('Halo! Terima Kasih Telah Berkunjung')
+  alert('Halo! Terima Kasih Telah Berkunjung');
 });
